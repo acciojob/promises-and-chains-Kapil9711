3169c6c0c37cbc223f3a7fc9a6b8a3defa58a5c2
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-24 09:45:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const ageInput = document.querySelector('#age');
+const nameInput = document.querySelector('#name');
+const submitBtn = document.querySelector('#btn');
+const validatorMessage = document.querySelector('#validator');
+
+validatorMessage.style.display = 'none';
+
+const isInputValid = () => nameInput.value && (ageInput.value || ageInput ===0);	
+
+const handleSubmit = ()=>{
+     return new Promise((resolve,reject)=>{
+		if(Number(ageInput.value) > 18) resolve(`Welcome, ${nameInput.value}. You can vote.`)
+		else reject(`Oh sorry ${nameInput.value}. You aren't old enough`);
+	})
+	
+	
+}
+
+
+submitBtn.addEventListener('click',(event)=>{
+	event.preventDefault();
+     validatorMessage.style.display = 'none';
+	const isValid = isInputValid();
+	if(!isValid) return validatorMessage.style.display = 'initial';
+	handleSubmit().then(msg=> window.alert(msg)).catch(msg=>window.alert(msg));
+});
+
+
+
+
+
+
+
+
